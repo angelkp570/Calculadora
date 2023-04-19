c452e8140e07b9281e0d62c7dcd1e4c2632d2836
angelkp570 <angel_ds@comunidad.unam.mx>
2023-04-19 00:07:42 -0600

    Se crea archivo funciones.h

--- calculadora.c
+++ calculadora.c
@@ -1,11 +1,38 @@
+//===========================================================================================
+// Programa: Presente en pantalla un menú con las siguientes opciones:
+//           1. Estadística básica
+//           2. Multiplicación de polinomios
+//           3. (Libre)
+//           0. Salir
+//           Selecciona una opción:
+//           - Si el usuario selecciona 1, el programa solicitará una serie de datos para
+//           trabajar y obtendrá la media aritmética, varianza y desviación estándar de
+//           los mismos.
+//           - Si elige 2, el programa solicitará el grado de los dos polinomios (diferentes),
+//           estableciendo los límites para los mismos (puede incluir el grado 0).
+//           Si se introducen datos válidos, el programa realizará la multiplicación, mostrará
+//           el resultado con formato de polinomio 
+// Autor:    Luis Angel Hernández Pascual
+//===========================================================================================
+
+//===========================================================================================
+// Librerias 
+//===========================================================================================
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+#include "funciones.h"
 
+//===========================================================================================
+// Variables Globales
+//===========================================================================================
 float a[80];
 int datos();
 void menu();
-// Funciones:
+
+//===========================================================================================
+// Declaración de Funciones
+//===========================================================================================
 float prom();
 float vari();
 int fact();
@@ -70,130 +97,4 @@ int main()
     return 0;
 }
 
-int MenuPrincipal()
-{
-    int opc;
-    do
-    {
-        printf("\n\n1. Estadistica basica\n");
-        printf("2. Multiplicacion de polinomios\n");
-        printf("3. Factorial\n");
-        printf("0. Salir\n");
-        scanf("%i", &opc);
-        if (opc < 0 || opc > 3)
-        {
-            printf("Opcion no valida\n");
-        }
 
-    } while (opc < 0 || opc > 3);
-    return opc;
-}
-
-int MenuEstadistica()
-{
-    int opc;
-    do
-    {
-        printf("1. Media aritmetica\n");
-        printf("2. Varianza\n");
-        printf("3. Desviacion estandar\n");
-        printf("0. Salir\n");
-        scanf("%i", &opc);
-        if (opc < 0 || opc > 3)
-        {
-            printf("Opcion no valida\n");
-        }
-
-    } while (opc < 0 || opc > 3);
-    return opc;
-}
-
-void menu(n)
-{ // system ("cls");
-    if (n == 1)
-    {
-        printf("\nMEDIA\n\n");
-    }
-    if (n == 2)
-    {
-        printf("\nVARIANZA\n\n");
-    }
-    if (n == 3)
-    {
-        printf("\nDESVIACION ESTANDAR\n\n");
-    }
-    if (n == 4)
-    {
-        printf("\nFactorial\n\n");
-    }
-}
-
-int datos(n)
-{
-    int cont, resp, respp;
-    // char resp;
-    printf("Conose la cantidad de datos?1/0");
-    scanf("%d", &resp);
-
-    if (resp == 1)
-    {
-        printf("\n\nIngrese el numero de datos:\t");
-        scanf("%d", &n);
-        printf("\n\nIngrese datos:");
-        for (cont = 1; cont <= n; cont++)
-        {
-            printf("\n%d.-", cont);
-            scanf("%f", &a[cont]);
-        }
-    }
-
-    if (resp == 0)
-    {
-        n = 0;
-        printf("\n");
-        do
-        {
-            n += 1;
-            printf("\nIngrese el valor %d:\n", n);
-            scanf("%f", &a[n]);
-            printf("Desea agregar otro valor?(1/0)");
-            scanf("%d", &respp);
-        } while (respp == 1);
-    }
-    return n;
-}
-
-float prom(n)
-{
-    int s;
-    float prom;
-    for (s = 1; s <= n; s++)
-    {
-        prom += a[s];
-    }
-    prom = prom / n;
-    return prom;
-}
-
-float vari(n)
-{
-    int cont;
-    float resultado = 0, pm;
-    pm = prom(n);
-    for (cont = 1; cont <= n; cont++)
-    {
-        resultado += (pow((a[cont] - pm), 2));
-    }
-    resultado /= n;
-    return resultado;
-}
-
-int fact(n)
-{
-    int cont, fac = 1;
-    for (cont = 1; cont <= n; cont++)
-    {
-        fac = fac * cont;
-    }
-    return fac;
-}
---
+++ funciones.h
@@ -0,0 +1,127 @@
+int MenuPrincipal()
+{
+    int opc;
+    do
+    {
+        printf("\n\n1. Estadistica basica\n");
+        printf("2. Multiplicacion de polinomios\n");
+        printf("3. Factorial\n");
+        printf("0. Salir\n");
+        scanf("%i", &opc);
+        if (opc < 0 || opc > 3)
+        {
+            printf("Opcion no valida\n");
+        }
+
+    } while (opc < 0 || opc > 3);
+    return opc;
+}
+
+int MenuEstadistica()
+{
+    int opc;
+    do
+    {
+        printf("1. Media aritmetica\n");
+        printf("2. Varianza\n");
+        printf("3. Desviacion estandar\n");
+        printf("0. Salir\n");
+        scanf("%i", &opc);
+        if (opc < 0 || opc > 3)
+        {
+            printf("Opcion no valida\n");
+        }
+
+    } while (opc < 0 || opc > 3);
+    return opc;
+}
+
+void menu(int n)
+{ // system ("cls");
+    if (n == 1)
+    {
+        printf("\nMEDIA\n\n");
+    }
+    if (n == 2)
+    {
+        printf("\nVARIANZA\n\n");
+    }
+    if (n == 3)
+    {
+        printf("\nDESVIACION ESTANDAR\n\n");
+    }
+    if (n == 4)
+    {
+        printf("\nFactorial\n\n");
+    }
+}
+
+int datos(int n)
+{
+    int cont, resp, respp;
+    // char resp;
+    printf("Conose la cantidad de datos?1/0");
+    scanf("%d", &resp);
+
+    if (resp == 1)
+    {
+        printf("\n\nIngrese el numero de datos:\t");
+        scanf("%d", &n);
+        printf("\n\nIngrese datos:");
+        for (cont = 1; cont <= n; cont++)
+        {
+            printf("\n%d.-", cont);
+            scanf("%f", &a[cont]);
+        }
+    }
+
+    if (resp == 0)
+    {
+        n = 0;
+        printf("\n");
+        do
+        {
+            n += 1;
+            printf("\nIngrese el valor %d:\n", n);
+            scanf("%f", &a[n]);
+            printf("Desea agregar otro valor?(1/0)");
+            scanf("%d", &respp);
+        } while (respp == 1);
+    }
+    return n;
+}
+
+float prom(int n)
+{
+    int s;
+    float prom;
+    for (s = 1; s <= n; s++)
+    {
+        prom += a[s];
+    }
+    prom = prom / n;
+    return prom;
+}
+
+float vari(int n)
+{
+    int cont;
+    float resultado = 0, pm;
+    pm = prom(n);
+    for (cont = 1; cont <= n; cont++)
+    {
+        resultado += (pow((a[cont] - pm), 2));
+    }
+    resultado /= n;
+    return resultado;
+}
+
+int fact(int n)
+{
+    int cont, fac = 1;
+    for (cont = 1; cont <= n; cont++)
+    {
+        fac = fac * cont;
+    }
+    return fac;
+}
